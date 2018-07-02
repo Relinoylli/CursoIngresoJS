3c043f37a7385f3118c51b1e1931295f18e19abb
Relinoylli <40763515+Relinoylli@users.noreply.github.com>
2018-07-02 12:07:05 -0300

    Ejercicios 1-3
    Los tres primeros ejercicios...
    PD: el archivo html del primero esta modificado para que mostrar este bien escrito

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,16 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
+	var Nombre;
+
+	// var person = prompt("Please enter your name", "Harry Potter"); fue sacado de w3cshool sobre alerts
+	Nombre = prompt("Ingrese su nombre", "Nombre Apellido");
 	
+	// tomo la Variable vDato y lo llamo el alert
+	// "//" es un comentario de una linea "/*" es un comentario en llave se cierra con "*/"
+	alert(" Su Nombre es : " + Nombre);
+
+	// concatenación de una texto entre comillas y la variable nombre y se lo manda atravez de un alert...
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,20 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	var nombre;
+	// document.getElementById(''); se consigue con g , tab , shitf + I
+	// busca un elemento HTML en base su ID
+	document.getElementById('elNombre');
+	alert(elNombre.value);
+	/*
+	alert("ejercicio tres");	
+	//console.log es un mensaje para consola...
+	console.log("ESTE MENSAJE ESTA PUESTO EN LA CONSOLA");
+
+
+	*/
+
 }
 
 
