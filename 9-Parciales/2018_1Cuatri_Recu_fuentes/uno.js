c608039925d33f2fa28c56e0be4c2e36f9fe8176
RicardoLinares <40763515+RicardoLinares@users.noreply.github.com>
2018-07-04 11:59:33 -0300

    Ejercicios recuperatorios 1-2
    Competencia en clase
    Primero : Completado, Dentro de los 10 primeros.
    Segundo: Completado, Afuera de los 10 primeros.

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,20 @@
 
-function mostrar()
+function Mostrar()
 {
+	// declaro las variables que voy a usar
+	var base;
+	var altura;
+	var perimetro;
+	var superficie;
 
+	base = prompt("base del triangulo", "0");
+	altura = prompt("altura del triangulo", "0");
+
+	parseInt(base);
+	parseInt(altura);
+
+	perimetro = base * 3;
+	superficie = (base * altura) / 2;
+
+	alert("El perimetro es " + perimetro + " y el la superficie es " + superficie);
 }
