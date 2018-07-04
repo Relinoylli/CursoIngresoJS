de9924fa92e6110f45a7b97f1fac9ea04e0b8358
RicardoLinares <40763515+RicardoLinares@users.noreply.github.com>
2018-07-04 12:22:55 -0300

    Ejercicios recuperatorios n3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,28 @@
-function mostrar()
+function Mostrar()
 {
-
+	// declaro las variables
+	var precio;
+	var descuento;
+	var descuentoTotal;
+	var precioDescontado;
+	var iva;
+	var precioFinal;
+	// uso prompt para asignar las variables precio y descuento
+	precio = prompt("Precio del producto","0");
+	descuento = prompt("Descuento a aplicar", "0");
+	// conversion de texto a num(opcional)
+	precio = parseInt(precio);
+	descuento = parseInt(descuento);
+	// calculo el descuento
+	descuentoTotal = (precio * descuento)/ 100;
+	// calculo el precion con el descuento
+	precioDescontado = precio - descuentoTotal;
+	// calculo el iva atravez del precio descontado 
+	iva = (precioDescontado * 21)/ 100;
+	// calculo el precio final
+	precioFinal = precioDescontado + iva;
+	// mando el mensaje al usuario con el descuento, precio descontado y el iva	
+	alert("Descuento: " + descuentoTotal + "$ \n" + "Precio con el descuento: " + precioDescontado + "$ \n" + "El iva: " + iva + "$");
+	// Escribo el resultado final en el textbox
+	document.getElementById('elPrecioFinal').value = precioFinal;
 }
