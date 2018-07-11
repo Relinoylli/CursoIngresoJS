98969a1df06c2904b0f43adc9ceef1437043eb73
RicardoLinares <40763515+RicardoLinares@users.noreply.github.com>
2018-07-11 12:23:09 -0300

    Ejercicio 1 - 3 Unidad 3

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,25 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
-
-
-
-
+	
+	var mesDelAño = document.getElementById('mes').value;
+	// usando Multi-case para resolver el enunciado
+	switch(mesDelAño){
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno.");
+			break;
+	    case "Julio":
+	    case "Agosto":
+			alert("Abrigate que hace frio.")
+			break;
+	    default:
+			alert("Ya pasamos el frio, ahora calor!!!")
+			break;
+    } 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,10 +1,16 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
+	//uso un switch para verificar si es febrero aunque pienso que en este caso seria mas effectivo un if-else doble
+	switch(mesDelAño){
+		case "Febrero":
+			alert("Este mes no tiene más de 29 días.");
+			break;
+		default:
+			alert("Este mes tiene 30 o más días");
+			break;
+	}
 
-//alert (mesDelAño);
-	
 	
 
 
