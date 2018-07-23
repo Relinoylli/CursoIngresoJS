9b587037869fb7f19d782b712fec4077821bf0d5
RicardoLinares <40763515+RicardoLinares@users.noreply.github.com>
2018-07-23 12:24:57 -0300

    Cuatro y Cinco

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+	var dia;
 
+	dia = prompt("Por favor ingrese un dia de la semana", "Lunes");
+
+	switch(dia)
+	{
+		case "Domingo":
+		case "Sabado":
+			alert("buen finde");
+			break;
+		case "Lunes":
+		case "Martes":
+		case "Miercoles":
+		case "Jueves":
+		case "Viernes":
+			alert("a trabajar");
+			break;
+		default:
+			alert("Por favor ingrese un dia valido \n Nota: recuerda usar mayusculas")
+	}
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,35 @@
 function mostrar()
 {
-
+	// defino las variables
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	// asigno los numeros atravez de un prompt
+	numeroUno = prompt("Porfavor eliga dos numeros","0");
+	numeroDos = prompt("Porfavor eliga dos numeros","0");
+	// y uso parseInt para convertilos a numero
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+	// y preguto si son iguales
+	if(numeroDos == numeroUno)
+	{
+		alert(numeroUno + " y " + numeroDos + " son iguales");
+	}
+	// sino pregunto si el primero es mayor
+	else if(numeroUno > numeroDos)
+	{
+		resultado = numeroUno / numeroDos;
+		alert(numeroUno + " divido " + numeroDos + " es igual a: " + resultado);
+	}
+	//sino sumo los numero
+	else
+	{
+		resultado = numeroUno + numeroDos;
+		alert(numeroUno + " mas " + numeroDos + " es igual a: " + resultado)
+		// y pregunto "es menor a 50?"
+		if(resultado < 50)
+		{
+			alert("La suma es " + resultado + " y es menor a 50");
+		}
+	}
 }
